55a593db6bf80e160b312a88312a2ccb115a084c
Utku Ablak <114539408+ablakutku@users.noreply.github.com>
2022-11-21 22:55:19 +0300

    Add files via upload

---
+++ python_examples.py
@@ -0,0 +1,49 @@
+# ODEV 1
+
+# x = input("Değer giriniz : ")
+# y = input("Değer giriniz : ")
+
+# if x == str(x) and y == str(y):
+#     print("İki değerde String yapıda")
+# if x==int(x) and y==int(y):
+#     print("İki değerde Integer ")
+
+#ODEV 2
+
+# vize = int(input("Vize notunuzu giriniz : "))
+# final = int(input("Final notunuzu giriniz : "))
+# ortalama = (vize*0.4)+(final*0.6)
+# print("Ortalamanız : ",ortalama)
+# if ortalama >= 90:
+#     print("AA")
+# elif ortalama >= 70:
+#     print("BB")
+# elif ortalama >= 60:
+#     print("CB")
+# elif ortalama >= 50:
+#     print("CC")
+
+
+#ODEV 3
+# secim = str(input("Hangi alana gireceksiniz? Tiyatro veya Sinema: "))
+# calisma = str(input("Çalışıyor musunuz? Evet veya Hayır: "))
+# ogrenci = str(input("Öğrenci misiniz? Evet veya Hayır: "))
+
+# sinema = 20
+# tiyatro = 15
+
+# if ogrenci == "Evet":
+#     sinema = sinema - (sinema*0.5)
+#     tiyatro = tiyatro - (tiyatro*0.5)
+#     if calisma == "Evet":
+#         sinema = sinema - (sinema*0.1)
+#         tiyatro = tiyatro - (tiyatro*0.1)
+#         if secim == "Sinema":
+#             print("Sinema bilet fiyatınız : ",sinema)
+#         elif secim == "Tiyatro":
+#             print("Tiyatro bilet fiyatınız : ",tiyatro)
+#     elif calisma == "Hayır":
+#         if secim == "Sinema":
+#             print("Sinema bilet fiyatınız : ",sinema)
+#         elif secim == "Tiyatro":
+#             print("Tiyatro bilet fiyatınız : ",tiyatro)
